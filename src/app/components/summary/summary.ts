76ef43f8d6484b71137c2c41e86efb7607a9e33c
christian godoy villalobos <cgodoy@defontana.com>
2025-09-04 11:04:19 -0400

    new changes

--- src/app/components/summary/summary.ts
+++ src/app/components/summary/summary.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, inject } from '@angular/core';
+import { DetailsService } from '@services/details';
+import { DividerModule } from 'primeng/divider';
 import { TabsModule } from 'primeng/tabs';
 
 @Component({
   selector: 'app-summary',
-  imports: [TabsModule],
+  imports: [CommonModule, TabsModule, DividerModule],
   templateUrl: './summary.html',
   styleUrl: './summary.scss'
 })
 export class Summary {
+  private details = inject(DetailsService);
 
+  resumen = this.details.resumen;
+  info = computed(() => this.resumen()?.info);
+  price = computed(() => this.resumen()?.price);
+
+  signClass(n: number) {
+    return n > 0 ? 'green' : n < 0 ? 'red' : '';
+  }
 }
